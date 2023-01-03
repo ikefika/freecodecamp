e33e595f8ed668eca25616b18d670870ef340a7c
Ikenna Bruce Efika <ikefika@yahoo.com>
2023-01-03 17:46:49 +0000

    Debugging section completed

---
+++ 2 - JavaScript Algorithms and Data Structures/4 - Debugging/errorchecklist.js
@@ -0,0 +1,13 @@
+// console.log(), and typeof() are two very common debug commands used. Others exist !!
+
+// Below are some common errors to check for during debugging
+
+// 1. Misspelled Variable and Function names
+// 2. Unclosed, Parentheses, Brackets, braces, Quotes
+// 3. Mixed usageof single and double quotes
+// 4. Use of Assignment Operator instead of equality operator
+// 5. Missing Open and Closing Parenthesis after a function call
+// 6. Arguments passed in the wrong order when calling a function
+// 7. Off by one Errors when Indexing
+// 8. Improper reinitialisation of variables inside a loop
+// 9. Infinite loops
