3f1efe016557681000c4228444ab5c398fe0963d
Ikenna Bruce Efika <ikefika@yahoo.com>
2023-01-11 21:31:57 +0000

    React (4/47)

---
+++ 3 - Front End Dev Libraries/4 - React/jsx_Basics.jsx
@@ -0,0 +1,17 @@
+const JSX = (
+    <div>
+        {/* Adding Comments in JSX */}
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+    </div>
+  );
+
+// Render HTML Elements to the DOM
+const JSX1 = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+  );
+  // Change code below this line
+  ReactDOM.render(JSX1, document.getElementById("challenge-node"));
