1a513dee80df97b0afbf0c7a0a4b7f7f32071ab9
Ikenna Bruce Efika <ikefika@yahoo.com>
2023-01-13 22:23:32 +0000

    React 12/47

---
+++ 3 - Front End Dev Libraries/4 - React/renderClasstoDOM.jsx
@@ -0,0 +1,23 @@
+/*Both the Fruits and Vegetables components are defined for you behind the scenes. 
+Render both components as children of the TypesOfFood component, then render TypesOfFood to the DOM. 
+There is a div with id='challenge-node' available for you to use.
+*/
+class TypesOfFood extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+          {/* Change code below this line */}
+          <Fruits />
+          <Vegetables />
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  };
+  
+  // Change code below this line
+  ReactDOM.render(<TypesOfFood />, document.getElementById("challenge-node"));
