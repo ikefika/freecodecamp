1726e4b7a8cdff2c0610318e145e5a4591688a22
Ikenna Bruce Efika <ikefika@yahoo.com>
2023-01-13 22:20:02 +0000

    Update createdNestedComponents.jsx

--- 3 - Front End Dev Libraries/4 - React/createdNestedComponents.jsx
+++ 3 - Front End Dev Libraries/4 - React/createdNestedComponents.jsx
@@ -44,3 +44,47 @@ const TypesOfFruit = () => {
       );
     }
   };
+
+
+
+
+
+  /* Compose React Components
+    Nest two components inside of Fruits — first NonCitrus, and then Citrus.
+    Both of these components are provided for you behind the scenes.
+    Next, nest the Fruits class component into the TypesOfFood component, below the h1 heading element and above Vegetables.
+    The result should be a series of nested components, which uses two different component types.
+  */
+  class Fruits1 extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h2>Fruits:</h2>
+          { /* Change code below this line */ }
+          <NonCitrus />
+          <Citrus />
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
+  
+  class TypesOfFood extends React.Component {
+    constructor(props) {
+       super(props);
+    }
+    render() {
+      return (
+        <div>
+          <h1>Types of Food:</h1>
+          { /* Change code below this line */ }
+          <Fruits1 />
+          { /* Change code above this line */ }
+          <Vegetables />
+        </div>
+      );
+    }
+  };
